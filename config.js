5d1ca461bc355aca02ba50e458cc2fe6458b75b1
Johannes Leander <johleander@gmail.com>
2020-05-03 22:46:21 +0200

    Uses Gatasy startet

---
+++ config.js
@@ -0,0 +1,32 @@
+module.exports = {
+  siteTitle: 'Johannes Leander', // <title>
+  manifestName: 'Resume',
+  manifestShortName: 'Landing', // max 12 characters
+  manifestStartUrl: '/',
+  manifestBackgroundColor: '#663399',
+  manifestThemeColor: '#663399',
+  manifestDisplay: 'standalone',
+  //manifestIcon: 'src/assets/img/website-icon.png',
+  //pathPrefix: `/gatsby-starter-resume/`, // This path is subpath of your hosting https://domain/portfolio
+  firstName: 'Johannes',
+  lastName: 'Leander',
+  // social
+  socialLinks: [
+    {
+      icon: 'fa-github',
+      name: 'Github',
+      url: 'https://github.com/johleander',
+    },
+    {
+      icon: 'fa-linkedin-in',
+      name: 'Linkedin',
+      url: 'https://linkedin.com/in/johleander/',
+    },
+    {
+      icon: 'fa-twitter',
+      name: 'Twitter',
+      url: 'https://twitter.com/johleander',
+    },
+  ],
+  email: 'johannes.leander@outlook.com',
+};
